170b77e5b692dd2c74a98fd0e22cd23aa4a509f1
Zhiyuan Luo <49680424+Morgenlefay@users.noreply.github.com>
2019-07-13 14:38:24 +0800

    Update Step5-GO-KEGG-Pathway.R

--- Step4: R/Step5-GO-KEGG-Pathway.R
+++ Step4: R/Step5-GO-KEGG-Pathway.R
@@ -1,16 +1,19 @@
 rm(list=ls())
 options(stringsAsFactors = F)
 library(clusterProfiler)
-library(DOSE)
+library(topGO)
+library(Rgraphviz)
+library(pathview)
 library(org.Hs.eg.db)
+library(DOSE)
 sig.gene<-read.csv(file="DEG.csv")
-head(sig.gene)
 gene<-sig.gene[,1]
-head(gene)
 gene.df<-bitr(gene, fromType = "ENSEMBL", 
               toType = c("SYMBOL","ENTREZID"),
               OrgDb = org.Hs.eg.db)
 head(gene.df)
+
+### GO Analysis
 ego_bp<-enrichGO(gene       = gene.df$ENSEMBL,
                  OrgDb      = org.Hs.eg.db,
                  keyType    = 'ENSEMBL',
@@ -20,3 +23,33 @@ ego_bp<-enrichGO(gene       = gene.df$ENSEMBL,
                  qvalueCutoff = 0.05)
 barplot(ego_bp,showCategory = 25,title="The GO_BP enrichment analysis of all DEGs ")
 dotplot(ego_bp,showCategory = 25,title="The GO_BP enrichment analysis of all DEGs ")
+plotGOgraph(ego_bp)
+ego_mf<-enrichGO(gene       = gene.df$ENSEMBL,
+                 OrgDb      = org.Hs.eg.db,
+                 keyType    = 'ENSEMBL',
+                 ont        = "MF",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff = 0.01,
+                 qvalueCutoff = 0.05)
+barplot(ego_mf,showCategory = 25,title="The GO_MF enrichment analysis of all DEGs")
+dotplot(ego_mf,showCategory = 25,title="The GO_MF enrichment analysis of all DEGs")
+plotGOgraph(ego_mf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
