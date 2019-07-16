d92a358ea7ac47f5881637c63b2dbae43273c92c
Zhiyuan Luo <49680424+Morgenlefay@users.noreply.github.com>
2019-07-16 15:38:17 +0800

    Update Step3-Heatmap.R

--- Step4: R/Step3-Heatmap.R
+++ Step4: R/Step3-Heatmap.R
@@ -24,3 +24,25 @@ n=t(scale(t(deg[cg,])))
 n[n>2]=2
 n[n< -2]= -2
 pheatmap(n,show_colnames =T,show_rownames = T)
+
+
+
+rm(list=ls())
+a<-read.csv(file="A315T.csv",header = T)
+b<-read.csv(file="A315T_DEG.csv",header = T)
+c<-merge(a,b,by=c("ID"))
+write.csv(c, file="Venn.csv",row.names = F)
+rm(list=ls())
+options(stringsAsFactors = F)
+dat <- read.csv("Venn.csv",header = TRUE,row.names = 1)
+deg <- dat[ ,-1:-6]
+x=dat$log2FoldChange
+names(x)=rownames(deg)
+cg=c(names(head(sort(x),200)),
+     names(tail(sort(x),200)))
+library(pheatmap)
+pheatmap(deg[cg,],show_colnames =T,show_rownames = T)
+n=t(scale(t(deg[cg,])))
+n[n>2]=2
+n[n< -2]= -2
+pheatmap(n,show_colnames =T,show_rownames = F)
