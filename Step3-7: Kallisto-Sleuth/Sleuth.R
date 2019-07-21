db0d422533cb2c2957e86c66d35c9f9c06faef1b
Zhiyuan Luo <49680424+Morgenlefay@users.noreply.github.com>
2019-07-21 18:57:09 +0800

    Update Sleuth.R

--- Step3-7: Kallisto-Sleuth/Sleuth.R
+++ Step3-7: Kallisto-Sleuth/Sleuth.R
@@ -1,3 +1,4 @@
+--------#### DEG 
 rm(list=ls())
 options(stringsAsFactors = F)
 library(rhdf5)
@@ -35,3 +36,67 @@ so <- sleuth_lrt(so, 'reduced', 'full')
 sleuth_table <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
 sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
 
+                   
+                   
+                   
+--------#### GO-KEGG-DO Analysis                   
+rm(list=ls())
+options(stringsAsFactors = F)
+library(clusterProfiler)
+library(topGO)
+library(Rgraphviz)
+library(pathview)
+library(org.Hs.eg.db)
+library(DOSE)
+sig.gene<-read.csv(file="DEG.csv")
+gene<-sig.gene[,2]
+gene.df<-bitr(gene, fromType = "ENSEMBL", 
+              toType = c("SYMBOL","ENTREZID"),
+              OrgDb = org.Hs.eg.db)
+head(gene.df)
+
+### GO Analysis
+ego_bp<-enrichGO(gene       = gene.df$ENSEMBL,
+                 OrgDb      = org.Hs.eg.db,
+                 keyType    = 'ENSEMBL',
+                 ont        = "BP",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff = 0.01,
+                 qvalueCutoff = 0.05)
+barplot(ego_bp,showCategory = 25,title="The GO_BP enrichment analysis of all DEGs ")
+dotplot(ego_bp,showCategory = 25,title="The GO_BP enrichment analysis of all DEGs ")
+plotGOgraph(ego_bp)
+ego_mf<-enrichGO(gene       = gene.df$ENSEMBL,
+                 OrgDb      = org.Hs.eg.db,
+                 keyType    = 'ENSEMBL',
+                 ont        = "MF",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff = 0.01,
+                 qvalueCutoff = 0.05)
+barplot(ego_mf,showCategory = 25,title="The GO_MF enrichment analysis of all DEGs")
+dotplot(ego_mf,showCategory = 25,title="The GO_MF enrichment analysis of all DEGs")
+plotGOgraph(ego_mf)
+
+### KEGG Analysis
+kegg<-gene.df$ENTREZID
+kk <- enrichKEGG(gene = kegg,
+                 organism = 'hsa',
+                 pvalueCutoff = 0.05,
+                 pAdjustMethod = "BH",
+                 qvalueCutoff = 0.05)
+dotplot(kk)
+barplot(kk)
+browseKEGG(kk, 'hsa04974')
+
+### DO Analysis
+do<-gene.df$SYMBOL
+do <- enrichDO(gene = kegg,
+               ont = "DO",
+               pvalueCutoff = 0.01,
+               pAdjustMethod = "BH",
+               minGSSize = 1,
+               maxGSSize = 500,
+               qvalueCutoff = 0.05,
+               readable = TRUE)
+dotplot(do)
+barplot(do)
